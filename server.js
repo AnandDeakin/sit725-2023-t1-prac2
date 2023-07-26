7ee5c3f2d9326f6ee9294fcefc170413e9234343
AnandDeakin <s223684301@deakin.edu.au>
2023-07-26 12:20:00 +1000

    Task 2.2 with get and post request

---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 5000;
+
+app.use(express.static(__dirname + '/'));
+
+// To parse the request Json without using parser middleware for post request
+app.use(express.json())
+
+app.get('/', function (req,res) {
+    res.render('index.html');
+});
+
+app.get('/addTwoNumbers', function (req,res) {
+    let num1 = req.query.num1;
+    let num2 = req.query.num2;
+    let result = parseInt(num1) + parseInt(num2);
+    let response = {data:result, message:'sucess', statusCode:200}
+    res.json(response);
+});
+
+
+app.post('/multiplyTwoNumbers',function(req,res){
+    const  {num1, num2 }= req.body;
+    let multipliedResult=parseInt(num1) * parseInt(num2);
+    let response = {data : multipliedResult, message:'success', statusCode:200}
+    res.json(response);
+});
+
+app.listen(port, ()=>{
+    console.log('express server started');
+});
